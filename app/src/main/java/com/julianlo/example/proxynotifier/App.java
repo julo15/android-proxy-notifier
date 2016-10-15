4655269ca5af0c0ab3c90dc8c49524597e669f2d
Julian Lo <julian.lo@gmail.com>
2016-10-15 16:22:41 -0700

    Hook up to Crashlytics

--- app/src/main/java/com/julianlo/example/proxynotifier/App.java
+++ app/src/main/java/com/julianlo/example/proxynotifier/App.java
@@ -2,6 +2,10 @@ package com.julianlo.example.proxynotifier;
 
 import android.app.Application;
 
+import com.crashlytics.android.Crashlytics;
+
+import io.fabric.sdk.android.Fabric;
+
 /**
  * Created by julianlo on 10/15/16.
  */
@@ -10,6 +14,7 @@ public class App extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        Fabric.with(this, new Crashlytics());
 
         final Preferences preferences = Preferences.getInstance(this);
         preferences.setFirstLaunchIfNecessary();
